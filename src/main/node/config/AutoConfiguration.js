61d91d6a1e60e12ad838af0e0905c1d815638d80
Giani Segatto <gsegatto@travix.com>
2019-06-28 19:08:31 +0100

    initial commit

---
+++ src/main/node/config/AutoConfiguration.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const Component = require("leap-core").Component;
+const MySqlDatasourceFactory = require("leap-web").AppBuilder;
+
+const ROUTER = "router";
+class AutoConfiguration {
+    
+    static preLoad(environment) {
+    }
+
+    static postLoad(components) {
+
+        const routers = components.filter(component => component.name.lowerCase.includes(ROUTER));
+
+        const appBuilder = new AppBuilder(express());
+        routers.forEach(router => appBuilder.addRouter(router));
+
+        return appBuilder.build();
+    }
+}
+
+module.exports = AutoConfiguration;
---
+++ src/main/node/functions.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const AutoConfiguration = require("./config/AutoConfiguration");
+
+exports = module.exports;
+
+exports.AutoConfiguration = AutoConfiguration;
